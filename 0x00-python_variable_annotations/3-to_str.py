c10d9ea8fa417296aaff1e81e147c9baf027394f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-07 10:48:38 +0200

    a function that returns a string representaion of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type annotaion function
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    args:
+     n: a float
+    returns: a string representation of a float
+    """
+    return n
