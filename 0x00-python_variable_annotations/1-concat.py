b0aa5b203782b1d13d2ddea24516c214c276e336
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-07 10:31:28 +0200

    a function that concatenate two function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Type annotaion function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    args:
+      str1: the first string
+      str2: the second string
+    return: the two string concatanated
+    """
+    return str1 + str2
