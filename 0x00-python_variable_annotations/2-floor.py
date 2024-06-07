2455b01e24c3f6ba6baed43baef51cc0f8686a19
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-07 10:42:06 +0200

    a function that returns a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Type annotation function
+"""
+
+
+def floor(n: float) -> int:
+    """
+    args:
+      n: a float
+    returns: the floor of the float
+    """
+    f = int(n)
+    return f
