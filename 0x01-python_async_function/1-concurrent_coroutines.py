f03c3d14e37458ef54f10d3b3776b8efdf78317f
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-10 13:37:56 +0200

    a function that return a list of concurrency and wait for their executing

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Multiple corountines
+"""
+
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+async def wait_n(n: int, max_delay: int):
+    """
+    args:
+       n: the number of time wait_rondom will be spawn
+       max_delay: the time
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*tasks)
