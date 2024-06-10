977b321ff5a0c0867dc49d3b2c96246fbe11f62c
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-10 20:38:16 +0200

    a function that returns a asyncio.task

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Multiple corountines
+"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    args:
+       n: the number of time wait_rondom will be spawn
+       max_delay: the time
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return await asyncio.gather(*tasks)
