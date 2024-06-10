56dc321c0508f5fdec821dae3a6c411fe3409936
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-10 21:10:47 +0200

    a function that returns a asyncio.task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,4 +14,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
        max_delay: the time
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    completed_delays = [await task for task in asyncio.as_completed(tasks)]
+    return completed_delays
