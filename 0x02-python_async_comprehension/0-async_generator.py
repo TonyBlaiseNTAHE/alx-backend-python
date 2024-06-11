3e4bca346b8c63cf78beb7e09b33b790fdb1b13e
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-11 21:20:38 +0200

    async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-import time
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """function
     """
     for _ in range(10):
