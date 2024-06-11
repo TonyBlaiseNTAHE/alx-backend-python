e7dd265209a24c9819395244a079e061306c1898
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-11 12:47:37 +0200

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async Generator
+"""
+
+import asyncio
+import random
+import time
+
+
+async def async_generator():
+    """function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
