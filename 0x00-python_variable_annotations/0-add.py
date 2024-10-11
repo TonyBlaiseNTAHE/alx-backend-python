697aef352c1c1dffd926be19f5c68effdccb3d57
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-11 23:27:07 +0200

    a function that add two number

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ a function that adding two number
+       a:  a float number
+       b: a float number
+       return: a float number
+    """
+    return a + b
