7dac98c5a81f0d5f46a54af715ddb5e45b660993
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-11 23:10:56 +0200

    a function that add two number

---
+++ 0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a function that add two float
+"""
+
+def add(a: float, b: float) -> float:
+    """add function
+    args:
+        a: float
+        b: float
+    returns:
+        a + b: float
+    """
+    return a + b
