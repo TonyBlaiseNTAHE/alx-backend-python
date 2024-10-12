68f3078616a8f6bda3711ca0800cce2cb5a136c4
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-10-12 13:39:35 +0200

    first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-"""Type annotation
+"""Duck typing - first element of a sequence
 """
+from typing import Sequence, Any, Union, Optional
 
-from typing import Any, Sequence, Union,
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any]:
-    """Type annotation
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Augment the code with correct duck-typed annotations
+    args:
+        lst: Sequence[Any]
+    return:
+        Union[Any, NoneType]
     """
     if lst:
         return lst[0]
